9bd5d2d4fc4ae0bb525a92f020a7026d45f3969e
Juan Placencia <juan.placencia.512@gmail.com>
2014-05-24 01:25:01 -0500

    Fix issue with generator.json for multiple Scaffolt commands

--- tasks/scaffold.js
+++ tasks/scaffold.js
@@ -20,29 +20,52 @@ module.exports = function(options) {
   // Iterate over non-module generators for creating tasks that scaffold
   desc('Scaffold item(s), or list available scaffolds');
   task('generate', function() {
-    var promises = [];
-
-    // Iterate over all available generators.
-    generators(generatorsPath).forEach(function(generator) {
-      var names = process.env[generator];
-      if(names) {
-        names.split(',').forEach(function(name) {
-          if(generator !== 'project' || name !== 'shared') {
-            promises.push(scaffoltP(generator, name, {
-              generatorsPath: generatorsPath,
-              revert: revert
-            }));
-          }
+    // Iterate over all available generators
+    var params = generators(generatorsPath)
+      .map(function(generator) {
+        return {
+          generator: generator,
+          names: process.env[generator]
+        };
+      })
+      .filter(function(param) {
+        return param.names;
+      })
+      .map(function(param) {
+        return param.names.split(',').map(function(name) {
+          return {
+            generator: param.generator,
+            name: name
+          };
         });
-      }
-    });
+      })
+      .reduce(function(a, b) {
+        return a.concat(b);
+      })
+      .filter(function(param) {
+        return param.generator !== 'project' || param.name !== 'shared';
+      });
 
-    // List available generators if no scaffolt commands were run
-    if(promises.length) {
-      return Promise.all(promises);
+    // Run scaffolt commands sequentially, clearing cache of generator.json
+    // at each step to avoid read-only errors
+    if(params.length) {
+      return Promise.reduce(params, function(initial, param) {
+        Object.keys(require.cache).filter(function(path) {
+          return path.endsWith('generator.json');
+        })
+        .forEach(function(path) {
+          delete require.cache[path];
+        });
+        return scaffoltP(param.generator, param.name, {
+          generatorsPath: generatorsPath,
+          revert: revert
+        });
+      }, 0);
     }
     else {
-      scaffolt.list({generatorsPath: generatorsPath});
+      scaffolt.list({
+        generatorsPath: generatorsPath
+      });
     }
   });
 
@@ -56,6 +79,7 @@ module.exports = function(options) {
 
 function generators(generatorsPath) {
   return fs.readdirSync(generatorsPath).filter(function(generator) {
-    return fs.existsSync(path.join(generatorsPath, generator, 'generator.json'));
+    generator = path.join(generatorsPath, generator, 'generator.json');
+    return fs.existsSync(generator);
   });
 }
