44717386d890ec7e5277daf2ee91cfe078d565f1
Juan Placencia <juan.placencia.512@gmail.com>
2014-05-27 09:30:55 -0500

    Add base web pack configuration

--- index.js
+++ index.js
@@ -1,37 +1,6 @@
 'use strict';
 
-require('sugar');
-var fs = require('fs');
-var path = require('path');
-var tasksDirectory = path.join(__dirname, 'tasks');
-
-module.exports = function(options) {
-  var rootPath = process.cwd();
-  options = options || {};
-
-  // If we are inside a project, set the project name
-  if(options.isProject) {
-    process.env.project = path.basename(process.cwd());
-  }
-
-  // Load tasks
-  tasks().forEach(function(task) {
-    require(path.join(tasksDirectory, task))(options);
-  });
-
-  // Set default task
-  task('default', function() {
-    jake.run('-T');
-  });
+module.exports = {
+  config: require('./config'),
+  tasks: require('./tasks')
 };
-
-function tasks() {
-  return fs
-  .readdirSync(tasksDirectory)
-  .filter(function(fileOrDir) {
-    return fs.statSync(path.join(tasksDirectory, fileOrDir)).isFile();
-  })
-  .filter(function(file) {
-    return file.endsWith('.js');
-  });
-}
---
+++ tasks/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+require('sugar');
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(options) {
+  var rootPath = process.cwd();
+  options = options || {};
+
+  // If we are inside a project, set the project name
+  if(options.isProject) {
+    process.env.project = path.basename(process.cwd());
+  }
+
+  // Load tasks
+  tasks().forEach(function(task) {
+    require(path.join(tasksDirectory, task))(options);
+  });
+
+  // Set default task
+  task('default', function() {
+    jake.run('-T');
+  });
+};
+
+function tasks() {
+  return fs
+  .readdirSync(__dirname)
+  .filter(function(fileOrDir) {
+    return fs.statSync(path.join(tasksDirectory, fileOrDir)).isFile();
+  })
+  .filter(function(file) {
+    return file.endsWith('.js');
+  })
+  .filter(function(file) {
+    return file !== 'index.js';
+  });
+}
